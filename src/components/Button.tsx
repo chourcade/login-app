d22ae6c9dea409873a8dcee65d6a63bd8ab1ebbc
Cristian Hourcade <chourcade@Cristians-MacBook-Pro.local>
2023-07-27 15:52:27 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+export const ButtonComponent = () => {
+    return <button style={{borderBottom:'10px dashed white', background:'#2a2a2a'}}>No hago nada, pero funciono ;)</button>
+}
+
+export default ButtonComponent;
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import ButtonComponent from "@/components/Button";
+
+export default function LoginPage () {
+    return (
+        <div>
+            <ButtonComponent/>
+        </div>
+    )
+}
